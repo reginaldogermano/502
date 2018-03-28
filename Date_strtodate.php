9d5d2238d661b455f53ab3d1058453ec66eb39e5
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2018-03-27 22:25:36 -0300

    First commi1t

---
+++ Date_strtodate.php
@@ -0,0 +1,6 @@
+<?php
+
+echo date("d/m/Y", strtotime("+1 day"));
+echo "<hr>";
+
+echo mktime(22,24,0,3,27,2018);
---
+++ Strtotime.php
@@ -0,0 +1,24 @@
+<?php
+
+
+echo strtotime("now");
+echo "<hr>";
+
+echo strtotime("27 March 2018");
+echo "<hr>";
+
+echo strtotime("+1 day");
+echo "<hr>";
+
+echo strtotime("+1 week");
+echo "<hr>";
+
+
+echo strtotime("+1 week 2 days");
+echo "<hr>";
+
+echo strtotime("next Turday");
+echo "<hr>";
+
+echo strtotime("last Monday");
+echo "<hr>";
